db8e8908858f08bd84fcfd76d1f6e23069b87957
Ryan Bezerra <ryandonascimentobezerra@gmail.com>
2022-09-14 17:07:47 -0300

    Todos os arquivos da fabrica

---
+++ Consultar/Dicas.py
@@ -0,0 +1,32 @@
+# Variaveis
+nome = 'teste $%@#$'                               #string
+numeroint = 12                                     #int
+numerofloat = 12.2                                 #float
+booleano = True #or False                           Boolean
+lista = ['Pedro' , 12]                             #lista
+dicionario = {'chave':'valor' , 'sorvete':'ninho'} #dicionario
+
+# Operadores
+'+ , - , * , / , // , %'
+
+'and , or , not'
+
+'< , > , <= , >= , == , !='
+
+# Estrutura de Decisão If
+#idade = 19
+
+#if idade < 18:
+    #print('Menor de idade')
+#elif idade == 18:
+    #print('Wtf man 18 kkkkkk')
+#else:
+    #print('Maior de idade')
+    
+# Estrutura de Repetição For
+for nomes in range(5):
+    print(nomes)
+    print('----')
+
+    
+    
